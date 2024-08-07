4786c79b507737c96d11df810f2cebf482f24a4f
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-08-07 18:20:21 -0300

    structure: Managercontroller strucutre

---
+++ src/control/ManagerController.ts
@@ -0,0 +1,8 @@
+import Animal from '../model/Animal';
+import Client from '../model/Client';
+
+export default class ManagerController {
+    getNewAnimal(client: Client, animal: Animal) {
+        return;
+    }
+}
